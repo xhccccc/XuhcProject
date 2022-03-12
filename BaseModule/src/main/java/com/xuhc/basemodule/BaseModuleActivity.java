bcd38e70285cc9d0ba4a67ba313e61b4fcc77650
xhc <xuhc@ktc.cn>
2022-03-12 14:48:34 +0800

    BaseModule模块增加新的startActivityForResult使用

--- BaseModule/src/main/java/com/xuhc/basemodule/BaseModuleActivity.java
+++ BaseModule/src/main/java/com/xuhc/basemodule/BaseModuleActivity.java
@@ -1,6 +1,9 @@
 package com.xuhc.basemodule;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +14,8 @@ import android.widget.Button;
 public class BaseModuleActivity extends BaseActivity implements View.OnClickListener, OnActivityResultManager.OnActivityResultCallBack {
 
     private static final String TAG = "xhccc" + BaseModuleActivity.class.getSimpleName();
+    
+    ActivityResultLauncher<Intent> intentActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,20 @@ public class BaseModuleActivity extends BaseActivity implements View.OnClickList
 
         Button btActivityResult = findViewById(R.id.bt_activity_result);
         btActivityResult.setOnClickListener(this);
+        Button btActivityResultNew = findViewById(R.id.bt_activity_result_new);
+        btActivityResultNew.setOnClickListener(this);
+
+        initActivityResult();
+    }
+
+    private void initActivityResult(){
+        ActivityResultContracts.StartActivityForResult mStartActivityForResult  = new ActivityResultContracts.StartActivityForResult();
+        intentActivityResultLauncher = registerForActivityResult(mStartActivityForResult, new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                Log.d(TAG,"result: " + result.toString());
+            }
+        });
     }
 
     @Override
@@ -32,6 +51,9 @@ public class BaseModuleActivity extends BaseActivity implements View.OnClickList
         if (id == R.id.bt_activity_result){
             Intent intent = new Intent(this, TestActivity.class);
             mOnActivityResultManager.startActivityForResult(intent,100,this);
+        } else if (id == R.id.bt_activity_result_new){
+            Intent intent = new Intent(this, TestActivity.class);
+            intentActivityResultLauncher.launch(intent);
         }
     }
 
@@ -39,5 +61,8 @@ public class BaseModuleActivity extends BaseActivity implements View.OnClickList
     public void onActivityResultCallBack(int requestCode, int resultCode, Intent data) {
         Log.d(TAG,"requestCode: " + requestCode);
         Log.d(TAG,"resultCode: " + resultCode);
+        if (data != null){
+            Log.d(TAG,"data: " + data.toString());
+        }
     }
 }
--- BaseModule/src/main/java/com/xuhc/basemodule/TestActivity.java
+++ BaseModule/src/main/java/com/xuhc/basemodule/TestActivity.java
@@ -1,10 +1,18 @@
 package com.xuhc.basemodule;
 
+import android.content.Intent;
+
 public class TestActivity extends BaseActivity {
 
     @Override
     protected BaseFragment getFragment() {
         return new TestFragment();
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(450, new Intent().putExtra("my-data", "data"));
+        super.onBackPressed();
     }
 
 }
