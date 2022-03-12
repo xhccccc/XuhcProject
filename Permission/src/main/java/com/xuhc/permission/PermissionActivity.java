e0a60312bb0240796249af72e6b00bc1a1b7c51f
xhc <xuhc@ktc.cn>
2022-03-12 15:17:21 +0800

    Permission模块调整;权限申请添加ActivityResultContracts方式,旧方式被标弃用

--- Permission/src/main/java/com/xuhc/permission/PermissionActivity.java
+++ Permission/src/main/java/com/xuhc/permission/PermissionActivity.java
@@ -1,14 +1,23 @@
 package com.xuhc.permission;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Map;
 
 public class PermissionActivity extends AppCompatActivity {
 
@@ -17,19 +26,64 @@ public class PermissionActivity extends AppCompatActivity {
     private TextView tvPermissionTip;
     public static final String[] STORAGE_PERMISSIONS = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
+    ActivityResultLauncher<String> requestPermissionActivityResultLauncher;
+    ActivityResultLauncher<String[]> requestPermissionActivityResultLauncher_multiple;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
-
         tvPermissionTip = findViewById(R.id.tv_permission_tip);
-        boolean hasPermission = PermissionUtils.checkPermission(this, STORAGE_PERMISSIONS,PermissionUtils.REQUEST_PERMISSION_CODE);
-        if (hasPermission){
-            //TODO
-            tvPermissionTip.setText("已授予权限");
-        }
+
+        //新方法
+        initActivityResult();
+        Button newRequestPermission = findViewById(R.id.bt_request_permission_new);
+        newRequestPermission.setOnClickListener(v -> {
+
+            //单个申请
+//            for (String needPermission : STORAGE_PERMISSIONS){
+//                requestPermissionActivityResultLauncher.launch(needPermission);
+//            }
+
+            //多个申请
+            requestPermissionActivityResultLauncher_multiple.launch(STORAGE_PERMISSIONS);
+        });
+
+        //旧方法
+        Button oldRequestPermission = findViewById(R.id.bt_request_permission_old);
+        oldRequestPermission.setOnClickListener(v -> {
+            boolean hasPermission = PermissionUtils.checkPermission(this, STORAGE_PERMISSIONS,PermissionUtils.REQUEST_PERMISSION_CODE);
+            if (hasPermission){
+                tvPermissionTip.setText("已授予权限");
+            }
+        });
+
     }
 
+    public void initActivityResult(){
+        //单个申请回调
+        ActivityResultContracts.RequestPermission requestPermission  = new ActivityResultContracts.RequestPermission();
+        requestPermissionActivityResultLauncher = registerForActivityResult(requestPermission, new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                Log.d(TAG, "One_ActivityResultContracts_onActivityResult: " + result);
+                tvPermissionTip.setText(result.toString());
+            }
+        });
+
+        //多个申请回调
+        ActivityResultContracts.RequestMultiplePermissions requestMultiplePermission  = new ActivityResultContracts.RequestMultiplePermissions();
+        requestPermissionActivityResultLauncher_multiple = registerForActivityResult(requestMultiplePermission, new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
+                Log.d(TAG, "Multiple_ActivityResultContracts_onActivityResult: " + result.toString());
+                tvPermissionTip.setText(result.toString());
+            }
+        });
+    }
+
+
+    //被弃用
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -47,10 +101,8 @@ public class PermissionActivity extends AppCompatActivity {
                 }
             }
             if (isCanRun){
-                //TODO
                 tvPermissionTip.setText("已授予权限——onRequestPermissionsResult");
             }
-
         }
     }
 }
